6d2593aabb58b24db5ab6c36401495dc4376c491
atmetz <atmetz@gmail.com>
2025-11-21 13:22:50 -0600

    added register user command

--- handler_user.go
+++ handler_user.go
@@ -1,16 +1,33 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"time"
+
+	"github.com/atmetz/rssgator/internal/database"
+	"github.com/google/uuid"
 )
 
+// Login function
 func handlerLogins(s *state, cmd command) error {
+	//set context for queries
+	con := context.Background()
 
+	// Verify the correct number of arguments
 	if len(cmd.Args) != 1 {
 		return fmt.Errorf("usage: %s <name>", cmd.Name)
 	}
 
-	err := s.cfg.SetUser(cmd.Args[0])
+	// Check if user exists
+	_, err := s.db.GetUser(con, cmd.Args[0])
+
+	if err != nil {
+		return fmt.Errorf("user does not exist: %v", err)
+	}
+
+	// Set current user to argument passed in
+	err = s.cfg.SetUser(cmd.Args[0])
 
 	if err != nil {
 		return fmt.Errorf("couldn't set current user: %v", err)
@@ -19,4 +36,50 @@ func handlerLogins(s *state, cmd command) error {
 	fmt.Println("User switched successfully!")
 
 	return nil
+}
+
+// Register user function
+func handlerRegister(s *state, cmd command) error {
+	//set context for queries
+	con := context.Background()
+
+	// Verify the correct number of arguments
+	if len(cmd.Args) != 1 {
+		return fmt.Errorf("usage: %s <name>", cmd.Name)
+	}
+
+	// Check if user exists
+	_, err := s.db.GetUser(con, cmd.Args[0])
+
+	if err == nil {
+		return fmt.Errorf("user already exists: %v", err)
+	}
+
+	// Create New User
+	createdUser, err := s.db.CreateUser(con, database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+		Name:      cmd.Args[0],
+	})
+
+	if err != nil {
+		return fmt.Errorf("error creating new user %s, %v", cmd.Args[0], err)
+	}
+
+	// Set current user to newly created user
+	err = s.cfg.SetUser(createdUser.Name)
+
+	if err != nil {
+		return fmt.Errorf("couldn't set current user: %v", err)
+	}
+
+	fmt.Println("User created successfully:")
+	printUser(createdUser)
+	return nil
+}
+
+func printUser(user database.User) {
+	fmt.Printf(" * ID:      %v\n", user.ID)
+	fmt.Printf(" * Name:    %v\n", user.Name)
 }
--- main.go
+++ main.go
@@ -1,37 +1,59 @@
 package main
 
 import (
-	//"fmt"
+	"database/sql"
 	"log"
 	"os"
 
+	_ "github.com/lib/pq"
+
 	"github.com/atmetz/rssgator/internal/config"
+	"github.com/atmetz/rssgator/internal/database"
 )
 
 type state struct {
+	db  *database.Queries
 	cfg *config.Config
 }
 
 func main() {
-	//fmt.Println("Working on commands")
+	// Read config file
 	cfg, err := config.Read()
 	if err != nil {
 		log.Fatalf("error reading config: %v", err)
 	}
 
+	//	Connect to database
+
+	dbURL := cfg.DBURL
+
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatalf("error opening database: %v", err)
+	}
+	defer db.Close()
+
+	dbQueries := database.New(db)
+
+	// create state
 	currentState := &state{
+		db:  dbQueries,
 		cfg: &cfg,
 	}
 
+	// Create cmds struct to hold registered commands
 	cmds := commands{
 		command: make(map[string]func(*state, command) error),
 	}
 
+	// Check for correct number of arguments
 	if len(os.Args) < 2 {
 		log.Fatalf("Usage: cli <command> [args...]")
 	}
 
+	// Register commands
 	cmds.register("login", handlerLogins)
+	cmds.register("register", handlerRegister)
 	err = cmds.run(currentState, command{Name: os.Args[1], Args: os.Args[2:]})
 
 	if err != nil {
