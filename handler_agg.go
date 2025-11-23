76d7828c5343ab32821c661d5dde3ecbbd7aba83
atmetz <atmetz@gmail.com>
2025-11-22 22:54:50 -0600

    updated to agg to rotate through followed feeds based on time interval

--- handler_agg.go
+++ handler_agg.go
@@ -3,18 +3,63 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
+	"time"
+
+	"github.com/atmetz/rssgator/internal/database"
 )
 
 func handlerAgg(s *state, cmd command) error {
-	feed := "https://www.wagslane.dev/index.xml"
-	// RSSfeed of feed variable
-	rssFeed, err := fetchFeed(context.Background(), feed)
-	if err != nil {
-		return fmt.Errorf("can not get feed from %s: %v", feed, err)
+
+	// Verify the correct number of arguments
+	if len(cmd.Args) != 1 {
+		return fmt.Errorf("usage: %s <name> <time duration, etc 1m>", cmd.Name)
 	}
 
-	fmt.Printf("Feed: %+v\n", rssFeed)
+	time_between_requests := cmd.Args[0]
 
-	return nil
+	timeBetweenRequests, err := time.ParseDuration(time_between_requests)
+
+	if err != nil {
+		return fmt.Errorf("invalide duration: %v", err)
+	}
+
+	fmt.Printf("Collecting feeds every %v...\n", timeBetweenRequests)
+
+	ticker := time.NewTicker(timeBetweenRequests)
+	for ; ; <-ticker.C {
+		scrapeFeeds(s)
+	}
+
+}
+
+func scrapeFeeds(s *state) {
+
+	nextFeed, err := s.db.GetNextFeedToFetch(context.Background())
+
+	if err != nil {
+		log.Println("cannot load next feed", err)
+	}
+	log.Println("Found a feed to fetch")
+	scrapeFeed(s.db, nextFeed)
+
+}
+
+func scrapeFeed(db *database.Queries, feed database.Feed) {
+	_, err := db.MarkFeedFetched(context.Background(), feed.ID)
+	if err != nil {
+		log.Printf("couldn't mark feed %s fecthed: %v", feed.Name, err)
+	}
+
+	rssFeed, err := fetchFeed(context.Background(), feed.Url)
+	if err != nil {
+		log.Printf("can not get feed %s: %v", feed.Url, err)
+	}
+
+	for _, item := range rssFeed.Channel.Item {
+		fmt.Printf("Found post: %s\n", item.Title)
+	}
+
+	log.Printf("Feed %s collected, %v posts found", feed.Name, len(rssFeed.Channel.Item))
 
 }
--- sql/schema/002_feeds.sql
+++ sql/schema/002_feeds.sql
@@ -1,8 +1,8 @@
 -- +goose Up
 CREATE TABLE feeds (
 id UUID PRIMARY KEY,
-created_at TIMESTAMP,
-updated_at TIMESTAMP,
+created_at TIMESTAMP NOT NULL,
+updated_at TIMESTAMP NOT NULL,
 name TEXT NOT NULL,
 url TEXT NOT NULL UNIQUE,
 user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE
