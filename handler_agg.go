75f3854a467590139179306fdc0f2ca36433d332
atmetz <atmetz@gmail.com>
2025-11-23 23:35:58 -0600

    add posts command to see recent posts

--- handler_agg.go
+++ handler_agg.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"fmt"
 	"log"
+	"strings"
 	"time"
 
 	"github.com/atmetz/rssgator/internal/database"
+	"github.com/google/uuid"
 )
 
 func handlerAgg(s *state, cmd command) error {
@@ -21,7 +24,7 @@ func handlerAgg(s *state, cmd command) error {
 	timeBetweenRequests, err := time.ParseDuration(time_between_requests)
 
 	if err != nil {
-		return fmt.Errorf("invalide duration: %v", err)
+		return fmt.Errorf("invalid duration: %v", err)
 	}
 
 	fmt.Printf("Collecting feeds every %v...\n", timeBetweenRequests)
@@ -56,10 +59,86 @@ func scrapeFeed(db *database.Queries, feed database.Feed) {
 		log.Printf("can not get feed %s: %v", feed.Url, err)
 	}
 
+	layouts := []string{
+		time.RFC1123Z,
+		time.RFC1123,
+		time.RFC822Z,
+		time.RFC822,
+		time.RFC3339,
+	}
+
 	for _, item := range rssFeed.Channel.Item {
-		fmt.Printf("Found post: %s\n", item.Title)
+
+		publishedAt := sql.NullTime{}
+		for _, l := range layouts {
+			if t, err := time.Parse(l, item.PubDate); err == nil {
+				publishedAt = sql.NullTime{
+					Time:  t,
+					Valid: true,
+				}
+			}
+		}
+
+		//fmt.Printf("Found post: %s\n", item.Title)
+		_, err = db.CreatePost(context.Background(), database.CreatePostParams{
+			ID:          uuid.New(),
+			CreatedAt:   time.Now().UTC(),
+			UpdatedAt:   time.Now().UTC(),
+			Title:       item.Title,
+			Url:         item.Link,
+			Description: sql.NullString{String: item.Description, Valid: true},
+			PublishedAt: publishedAt,
+			FeedID:      feed.ID,
+		})
+
+		if err != nil {
+			if strings.Contains(err.Error(), "duplicate key value violates unique constraint") {
+				// duplicate key — ignore
+				continue
+			} else {
+				log.Printf("create post error: %v", err)
+				continue
+			}
+		}
 	}
 
 	log.Printf("Feed %s collected, %v posts found", feed.Name, len(rssFeed.Channel.Item))
 
+}
+
+func handlerBrowse(s *state, cmd command, user database.User) error {
+
+	if len(cmd.Args) > 2 {
+		return fmt.Errorf("usage: %s <limit, default 2>", cmd.Name)
+	}
+
+	var limit int32
+	limit = 2
+
+	if len(cmd.Args) == 1 {
+		_, err := fmt.Sscanf(cmd.Args[0], "%d", &limit)
+		if err != nil {
+			limit = 2
+		}
+	}
+
+	posts, err := s.db.GetPostsForUser(context.Background(), database.GetPostsForUserParams{
+		UserID: user.ID,
+		Limit:  limit,
+	})
+
+	if err != nil {
+		return fmt.Errorf("cannot browse posts: %v", err)
+	}
+
+	fmt.Printf("%s's most recent followed posts:\n", user.Name)
+	for _, post := range posts {
+		fmt.Printf("%s from %s\n", post.PublishedAt.Time.Format("Mon Jan 2"), post.FeedName)
+		fmt.Printf("--- %s ---\n", post.Title)
+		fmt.Printf("    %v\n", post.Description.String)
+		fmt.Printf("Link: %s\n", post.Url)
+		fmt.Println("=====================================")
+	}
+
+	return nil
 }
